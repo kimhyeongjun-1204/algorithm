c25b16c50971f13509fd70c6907d939de5add37e
kimhyeongjun-1204 <kl1533@gachon.ac.kr>
2026-04-14 13:31:53 +0900

    [level 3] Title: 보행자 천국, Time: 75.05 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
+++ 프로그래머스/3/1832. 보행자 천국/보행자 천국.java
@@ -1,34 +1,25 @@
 class Solution {
-    int MOD = 20170805;
+  int MOD = 20170805;
+  public int solution(int m, int n, int[][] cityMap) {
+        int answer = 0;
+        int[][] right = new int[m + 1][n + 1];
+        int[][] down = new int[m + 1][n + 1];
 
-    public int solution(int m, int n, int[][] cityMap) {
-        int[][] right = new int[m][n]; // 오른쪽으로 진행하며 도착
-        int[][] down  = new int[m][n]; // 아래로 진행하며 도착
+        right[1][1] = 1;
+        down[1][1] = 1;
 
-        right[0][0] = 1;
-        down[0][0] = 1;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0) continue;
-                if (cityMap[i][j] == 1) continue;
-
-                int fromUp   = (i > 0) ? down[i-1][j] : 0;
-                int fromLeft = (j > 0) ? right[i][j-1] : 0;
-
-                if (cityMap[i][j] == 0) {
-                    down[i][j]  = (fromUp + fromLeft) % MOD;
-                    right[i][j] = (fromUp + fromLeft) % MOD;
-                } else { // 2: 직진만
-                    down[i][j]  = fromUp;
-                    right[i][j] = fromLeft;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (cityMap[i - 1][j - 1] == 0) {
+                    right[i][j] += (right[i][j - 1] + down[i - 1][j]) % MOD;
+                    down[i][j] += (down[i - 1][j] + right[i][j - 1]) % MOD;
+                } else if (cityMap[i - 1][j - 1] == 2) {
+                    right[i][j] = right[i][j - 1];
+                    down[i][j] = down[i - 1][j];
                 }
             }
         }
-
-        if (m == 1 && n == 1) return 1;
-        int fromUp   = (m > 1) ? down[m-2][n-1] : 0;
-        int fromLeft = (n > 1) ? right[m-1][n-2] : 0;
-        return (fromUp + fromLeft) % MOD;
+        answer = (right[m][n - 1] + down[m - 1][n]) % MOD;
+        return answer;
     }
 }
